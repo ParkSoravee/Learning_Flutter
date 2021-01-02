f1a796542dc063cc3712398cb5d84661a0bdfd9d
Park Soravee <park11102001@gmail.com>
2021-01-02 14:12:52 +0700

    Expanded with Row (to config flex)

--- lib/main.dart
+++ lib/main.dart
@@ -9,39 +9,30 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Stack Layout",
+      title: "Expanded",
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Stack Layout"),
+          title: Text("Expanded"),
         ),
-        body: Stack(
-          alignment: Alignment(0.6, 0.6),
-          children: [
-            CircleAvatar(
-              backgroundImage: NetworkImage(
-                  'https://d25tv1xepz39hi.cloudfront.net/2017-08-21/files/landscape-photography_1645.jpg',),
-              radius: 110,
-            ),
-            // Image.network(
-            //   'https://d25tv1xepz39hi.cloudfront.net/2017-08-21/files/landscape-photography_1645.jpg',
-            //   fit: BoxFit.cover,
-            // ),
-            Container(
-              padding: EdgeInsets.fromLTRB(12, 4, 12, 4),
-              decoration: BoxDecoration(
-                color: Colors.black45,
-                borderRadius: BorderRadius.circular(4),
-              ),
-              child: Text(
-                "Beautiful view",
-                style: TextStyle(
-                  fontSize: 15,
-                  color: Colors.white,
-                  fontWeight: FontWeight.w600,
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Row(children: [
+            Expanded(
+              flex: 2,
+              child: Container(
+                child: Text(
+                  "Food",
+                  style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
                 ),
               ),
-            )
-          ],
+            ),
+            Container(
+              child: RaisedButton(
+                onPressed: () {},
+                child: Text("View more"),
+              ),
+            ),
+          ]),
         ),
       ),
     );
